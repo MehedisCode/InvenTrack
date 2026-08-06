068f924d49533917fd5754b764a44f83159287f2
Md Mehedi Hasan <mehedihasan.cse.2002@gmail.com>
2026-08-06 10:26:36 +0600

    feat(logging): integrate Serilog for enhanced logging capabilities

--- src/InvenTrack.API/Program.cs
+++ src/InvenTrack.API/Program.cs
@@ -2,73 +2,98 @@ using InvenTrack.API.Middleware;
 using InvenTrack.Application;
 using InvenTrack.Infrastructure;
 using Microsoft.OpenApi;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-builder.Services.AddApplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+try
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "InvenTrack API",
-        Version = "v1",
-        Description = "Inventory Management System API with JWT Authentication"
-    });
+    Log.Information("Starting web application");
 
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\nEnter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1Ni...\""
-    });
+    var builder = WebApplication.CreateBuilder(args);
 
-    options.AddSecurityRequirement((doc) => new OpenApiSecurityRequirement
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
+
+    // Add services to the container.
+    builder.Services.AddApplicationServices();
+    builder.Services.AddInfrastructureServices(builder.Configuration);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
     {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "InvenTrack API",
+            Version = "v1",
+            Description = "Inventory Management System API with JWT Authentication"
+        });
+
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme.\r\n\r\nEnter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1Ni...\""
+        });
+
+        options.AddSecurityRequirement((doc) => new OpenApiSecurityRequirement
+        {
         { new OpenApiSecuritySchemeReference("Bearer", doc), new List<string>() }
+        });
+
+        var apiXmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var apiXmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, apiXmlFile);
+        if (System.IO.File.Exists(apiXmlPath))
+        {
+            options.IncludeXmlComments(apiXmlPath);
+        }
+
+        var appXmlFile = "InvenTrack.Application.xml";
+        var appXmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, appXmlFile);
+        if (System.IO.File.Exists(appXmlPath))
+        {
+            options.IncludeXmlComments(appXmlPath);
+        }
     });
 
-    var apiXmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var apiXmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, apiXmlFile);
-    if (System.IO.File.Exists(apiXmlPath))
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        options.IncludeXmlComments(apiXmlPath);
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "InvenTrack API v1");
+        });
     }
 
-    var appXmlFile = "InvenTrack.Application.xml";
-    var appXmlPath = System.IO.Path.Combine(AppContext.BaseDirectory, appXmlFile);
-    if (System.IO.File.Exists(appXmlPath))
-    {
-        options.IncludeXmlComments(appXmlPath);
-    }
-});
+    app.UseHttpsRedirection();
 
-var app = builder.Build();
+    app.UseSerilogRequestLogging();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "InvenTrack API v1");
-    });
+    app.UseMiddleware<ExceptionMiddleware>();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
